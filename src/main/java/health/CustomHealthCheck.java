4a901354d0234b6ae6272e4ea592a98e4ce61062
Michael Sommer <michael.sommer@exxcellent.de>
2021-02-17 15:45:53 +0100

    remove unused import

--- src/main/java/health/CustomHealthCheck.java
+++ src/main/java/health/CustomHealthCheck.java
@@ -3,7 +3,6 @@ package health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
-import org.eclipse.microprofile.health.Liveness;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
